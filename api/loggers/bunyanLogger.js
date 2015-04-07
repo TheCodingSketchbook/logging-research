85ffcbb80ab2a1042213580d3f9fb8e84681f300
indigo0086 <indigo0086@gmail.com>
2015-04-07 09:51:51 -0400

    adding bunyan logger examples

--- api/loggers/bunyanLogger.js
+++ api/loggers/bunyanLogger.js
@@ -1,6 +1,18 @@
 (function() {
     var bunyan = require('bunyan');
 
-    var log = jbunyan.createLogger({name: 'appLogger'});
-    module.exports = log;
+    var logger = bunyan.createLogger({
+        name: 'request-log',
+        streams: [
+            {
+                level: 'info',
+                stream: process.stdout
+            },
+            {
+                level: 'error',
+                path: '/bunyan-logs/bunyan-error.log'
+            }
+        ]
+    });
+    module.exports = logger;
 })();
--- api/routes.js
+++ api/routes.js
@@ -5,51 +5,79 @@
 
     //Winston Routes
     var winstonLogger = require('./loggers').winston;
-    var winstonQuery = require('./loggers').winstonQuery;
-
     server.route([
-    {
-        method: 'GET',
-        path: '/winston-success/{message}',
-        handler: function(request, reply) {
-            var message = request.params.message;
+        {
+            method: 'GET',
+            path: '/winston-success/{message}',
+            handler: function(request, reply) {
+                var message = request.params.message;
 
-            //Built in string interpolation
-            winstonLogger.log('debug', 'requesting [%s]', message);
-            reply({message: message});
-        }
-    },
-    {
-        method: 'GET',
-        path: '/winston-error/{message}',
-        handler: function(request, reply) {
-            var message = request.params.message;
-            var data = null;
-
-            winstonLogger.log('info', 'making request with data: %j', {message: message}, {});
-            if(message == 'data') {
-                data = {details: 'incorrect arguments passed'};
+                //Built in string interpolation
+                winstonLogger.log('debug', 'requesting [%s]', message);
+                reply({message: message});
             }
+        },
+        {
+            method: 'GET',
+            path: '/winston-error/{message}',
+            handler: function(request, reply) {
+                var message = request.params.message;
+                var data = null;
 
-            winstonLogger.log("error", "Bad Request made", data);
-            reply.continue();
-        }
-    },
-    {
-        method: 'GET',
-        path: '/winston-query/type',
-        handler: function(request, reply) {
-            var query = {
-                from: new Date() - 24 * 60 * 60 * 1000,
-                until: new Date(),
-                limit: 10,
-                start: 0,
-                order: 'desc'
-            };
+                winstonLogger.log('info', 'making request with data: %j', {message: message}, {});
+                if(message == 'data') {
+                    data = {details: 'incorrect arguments passed'};
+                }
 
-            winstonLogger.query(query, function(err, results) {
-                reply(results);
-            });
+                winstonLogger.log("error", "Bad Request made", data);
+                reply.continue();
+            }
+        },
+        {
+            method: 'GET',
+            path: '/winston-query',
+            handler: function(request, reply) {
+                var query = {
+                    from: new Date() - 24 * 60 * 60 * 1000,
+                    until: new Date(),
+                    limit: 10,
+                    start: 0,
+                    order: 'desc'
+                };
+
+                winstonLogger.query(query, function(err, results) {
+                    reply(results);
+                });
+            }
+        }]);
+
+    //Bunyan Routes
+    var bunyanLogger = require('./loggers').bunyan;
+    server.route([
+        {
+            method: 'GET',
+            path: '/bunyan-success/{message}',
+            handler: function(request, reply) {
+                var message = request.params.message;
+                bunyanLogger.info('User has sent the message: %j', message);
+                reply({message: message});
+            }
+        },
+        {
+            method: 'GET',
+            path: '/bunyan-error/{message}',
+            handler: function(request, reply) {
+                var message = request.params.message;
+                var data = null;
+
+                bunyanLogger.info('making request with data: %j', {message: message});
+                if(message == 'data') {
+                    data = {details: 'incorrect arguments passed'};
+                    bunyanLogger.error("Bad Request made", data);
+                }
+
+                reply.continue();
+            }
         }
-    }]);
+    ]);
 })();
